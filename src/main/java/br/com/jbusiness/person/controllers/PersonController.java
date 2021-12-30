797cfcae0f688a1a56717914f61e80d4cdfef8da
rodrigoks <rodrigoks@gmail.com>
2021-12-30 12:09:37 -0300

    Implementados os verbos GET, POST, PUT e DELETE.

--- src/main/java/br/com/jbusiness/person/controllers/PersonController.java
+++ src/main/java/br/com/jbusiness/person/controllers/PersonController.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +29,21 @@ public class PersonController {
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public Person findById(@PathVariable("id") String id) throws UnsuportedMathException {
 		return services.findById(id);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Person create(@RequestBody Person person) throws UnsuportedMathException {
+		return services.create(person);
+	}
+	
+	@RequestMapping(method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Person update(@RequestBody Person person) throws UnsuportedMathException {
+		return services.update(person);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public void delete(@PathVariable("id") String id) throws UnsuportedMathException {
+		services.delete(id);
 	}
 	
 }
